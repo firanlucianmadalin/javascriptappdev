e815d6ca10734291ffa11661ffcdac69375cb566
firanlucianmadalin <95710495+firanlucianmadalin@users.noreply.github.com>
2022-06-23 20:44:26 +0300

    'curs5'

--- vuecrudd/frontend/src/components/NavBar.vue
+++ vuecrudd/frontend/src/components/NavBar.vue
@@ -4,7 +4,7 @@
         <router-link to="/" class="navbar-brand">CRUD Vue</router-link>
         <div class="navbar-nav mr-auto">
           <li class="nav-item">
-            <router-link to="/users" class="nav-link">Home</router-link>
+            <router-link to="/users" class="nav-link">Home22</router-link>
           </li>
           <li class="nav-item">
             <router-link to="/add" class="nav-link">Add User</router-link>
